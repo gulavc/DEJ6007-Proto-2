f31f792755ec7d28849ed60f43d26f3bb60451a6
marc-olivier viboux <Mviboux@hotmail.com>
2018-11-21 11:28:16 -0500

    J'ai dis allo au monde

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-
+    //Hello world
     //References to other scripts
     public PlayerInputManager playerInput;
 

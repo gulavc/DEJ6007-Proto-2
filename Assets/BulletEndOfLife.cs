53da8f5c6e42e9441310bb46fe434040d0a66e94
marc-olivier viboux <Mviboux@hotmail.com>
2018-12-11 06:29:20 -0500

    J'ai fait les murs de la guitare, et j'ai rajouter le code pour que les balles fasse du degat au joueur.

---
+++ Assets/BulletEndOfLife.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletEndOfLife : MonoBehaviour {
+
+    private void OnColliderEnter(Collider other)
+    {
+        if (other.gameObject.tag == "BulletStop")
+        {
+            Destroy(this.gameObject);
+        }
+            }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<PlayerController>().RemoveHP(10);
+            Destroy(this.gameObject);
+        }
+
+        if (collision.gameObject.tag == "Enemy")
+        {
+            collision.gameObject.GetComponent<PlayerController>().RemoveHP(10);
+            Destroy(this.gameObject);
+        }
+    }
+}

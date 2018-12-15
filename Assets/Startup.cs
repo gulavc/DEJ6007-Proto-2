995b58c05e3f623a93b2c30b9461e40112aed2bb
Patrick Paquin <patrick.paquin002@gmail.com>
2018-12-14 22:35:08 -0500

    ok

---
+++ Assets/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+
+public class Startup : MonoBehaviour {
+    public GameObject ControlsUI;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void StartGame()
+    { 
+       
+        SceneManager.LoadScene("iLevelDesign");
+        Debug.Log("Ca part en salade");
+    }
+
+    public void ControlsScreen()
+    {
+        ControlsUI.SetActive(true);
+    }
+
+    public void ControlsReturn()
+    {
+        ControlsUI.SetActive(false);
+    }
+}

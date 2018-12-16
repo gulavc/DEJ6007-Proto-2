98ef33b9c8697d41a5a6aa7d66c1c62808a82eb7
Capi <glavoieh@gmail.com>
2018-12-15 20:47:41 -0500

    victory zone test

---
+++ Assets/VictoryZone.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryZone : MonoBehaviour {
+
+    private bool victory = false;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (victory)
+        {
+            //TODO: Appeler la fonction du vicotry UI qui affiche la victoire
+
+
+            victory = false;
+        }
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            victory = true;
+        }
+    }
+}

69c21827c8c544997c79d89c1b65c6b4b27439e2
Patrick Paquin <patrick.paquin002@gmail.com>
2018-12-16 10:52:32 -0500

    ok

---
+++ Assets/GunUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GunUI : MonoBehaviour {
+
+    public GameObject gun1text;
+    public GameObject GunItem1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+		
+	}
+
+    void Gun1()
+    {
+        gun1text.GetComponent<Text>().color = Color.yellow;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Gun1();      
+                }
+    }
+
+
+}
